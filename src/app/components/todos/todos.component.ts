22800931d853feb0721c0ead0987a683f2ae270e
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2020-12-10 09:28:44 +0100

    model added

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todos: Todo[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: "Todo One",
+        completed: false
+      },
+    ]
   }
 
 }
